4418ab6df07f3dd24adea4d52b36a72e6310862a
claytonsavage <clytnsavage@gmail.com>
2017-12-05 01:58:10 +0000

    added the print each avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,28 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-}
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization: "token " + secrets.GITHUB_TOKEN,
+    },
+    json : true
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body)
+      body.forEach(function(item){
+        console.log(item.avatar_url)
+      });
+    });
+  };
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
