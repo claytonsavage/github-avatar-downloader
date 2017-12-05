cb12f64e3aa021b9aa9916d6fc1a195dd1a8cd01
claytonsavage <clytnsavage@gmail.com>
2017-12-05 05:01:53 +0000

    final changes

--- download_avatars.js
+++ download_avatars.js
@@ -8,8 +8,12 @@ var repoName = process.argv[3];
 //welcome message
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var border = function(){
+  console.log('------------------------------------');
+};
+
 //check that there is no missing information
-if (repoOwner != undefined && repoName != undefined) {
+if (repoOwner !== undefined && repoName !== undefined) {
 //download the image to the avatars folder
   console.log("Downloading...");
 
@@ -26,7 +30,7 @@ if (repoOwner != undefined && repoName != undefined) {
       .pipe(fs.createWriteStream(filePath));
   }
 
-//get the repo contributors api based on the name of the repo and the name of the owner of that repo
+  //get the repo contributors api based on the name of the repo and the name of the owner of that repo
   function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -36,15 +40,15 @@ if (repoOwner != undefined && repoName != undefined) {
       },
       json: true
     };
-//download the image to the avatars folder using the users login for the name
+    //download the image to the avatars folder using the users login for the name
     request(options, function(err, res, body) {
       cb(err, body);
-      if (body.message != 'Not Found') {
-      body.forEach(function(item){
-        avatarUrl = item.avatar_url;
-//call downloadImageByURL function
-        downloadImageByURL(avatarUrl, 'avatars/' + item.login + '.jpeg');
-      });
+      if (body.message !== 'Not Found') {
+        body.forEach(function(item){
+          avatarUrl = item.avatar_url;
+          //call downloadImageByURL function
+          downloadImageByURL(avatarUrl, 'avatars/' + item.login + '.jpeg');
+        });
       } else {
         border();
         console.log('ERROR: Check your repo or owner name');
@@ -52,13 +56,10 @@ if (repoOwner != undefined && repoName != undefined) {
       }
     });
   }
-var border = function(){
-  console.log('------------------------------------')
-}
 
-//call the function
+  //call the function
   getRepoContributors(repoOwner, repoName, function() {});
 // if information missing tell the user to add the repo owner and name
 } else {
-  console.log('Please define repo owner and name...')
+  console.log('Please define repo owner and name...');
 }
