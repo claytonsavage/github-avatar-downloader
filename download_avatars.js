7401a8cab7f390f45861c49dbbf888344fb9e55d
claytonsavage <clytnsavage@gmail.com>
2017-12-05 02:27:18 +0000

    save a avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var secrets = require('./secrets.js');
+var fs = require('fs');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,17 +14,32 @@ function getRepoContributors(repoOwner, repoName, cb) {
     },
     json : true
   };
-
+  var avatarUrl = '';
   request(options, function(err, res, body) {
     cb(err, body)
       body.forEach(function(item){
-        console.log(item.avatar_url)
+        avatarUrl = item.avatar_url;
+        console.log(avatarUrl);
       });
     });
   };
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Result:", result);
 });
 
+function downloadImageByURL(url, filePath) {
+  request.get("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+  .on('error', function (err) {
+          throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log((response.statusMessage), (response.headers['content-type']));
+         console.log('Downloading image...');
+         console.log('Download complete.');
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));
+}
+
+downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', 'avatars/kvirani.jpg');
