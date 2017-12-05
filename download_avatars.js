3a764d7e8ed536feea7620de8a58768659b5278d
claytonsavage <clytnsavage@gmail.com>
2017-12-05 02:49:35 +0000

    linted the download avatars file

--- download_avatars.js
+++ download_avatars.js
@@ -5,40 +5,41 @@ var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function downloadImageByURL(url, filePath) {
+  request.get(url, filePath)
+    .on('error', function (err) {
+      console.log(err);
+      throw err;
+    })
+    .on('response', function (response) {
+      console.log((response.statusMessage), (response.headers['content-type']));
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
+
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      Authorization: "token " + secrets.GITHUB_TOKEN,
+      Authorization: "token " + secrets.GITHUB_TOKEN
     },
-    json : true
+    json: true
   };
   var avatarUrl = '';
 
   request(options, function(err, res, body) {
-    cb(err, body)
-      body.forEach(function(item){
-        avatarUrl = item.avatar_url;
+    cb(err, body);
+    body.forEach(function(item){
+      avatarUrl = item.avatar_url;
 
-        downloadImageByURL(avatarUrl, 'avatars/' + item.login + '.jpeg');
-      });
+      downloadImageByURL(avatarUrl, 'avatars/' + item.login + '.jpeg');
     });
-  };
+  });
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
 
-function downloadImageByURL(url, filePath) {
-  request.get(url, filePath)
-  .on('error', function (err) {
-          console.log(err);
-          throw err;
-       })
-       .on('response', function (response) {                           // Note 3
-         console.log((response.statusMessage), (response.headers['content-type']));
-       })
-       .pipe(fs.createWriteStream(filePath));
-}
