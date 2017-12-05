9cbd27ef6fb3357810f3920e5ae55ac35c8cbde3
claytonsavage <clytnsavage@gmail.com>
2017-12-05 04:15:44 +0000

    refactoring code

--- download_avatars.js
+++ download_avatars.js
@@ -1,45 +1,51 @@
+//define variables and requirements
 var request = require('request');
 var secrets = require('./secrets.js');
 var fs = require('fs');
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+//welcome message
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//check that there is no missing information
 if (repoOwner != undefined && repoName != undefined) {
-function downloadImageByURL(url, filePath) {
-  request.get(url, filePath)
-    .on('error', function (err) {
-      console.log(err);
-      throw err;
-    })
-    .on('response', function (response) {
-      console.log((response.statusMessage), (response.headers['content-type']));
-    })
-    .pipe(fs.createWriteStream(filePath));
-}
-
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      Authorization: "token " + secrets.GITHUB_TOKEN
-    },
-    json: true
-  };
-
-  request(options, function(err, res, body) {
-    cb(err, body);
-    body.forEach(function(item){
-      avatarUrl = item.avatar_url;
-      downloadImageByURL(avatarUrl, 'avatars/' + item.login + '.jpeg');
-    });
-  });
-}
-
-getRepoContributors(repoOwner, repoName, function(err, result) {
+//download the image to the avatars folder
   console.log("Downloading...");
-});
+  function downloadImageByURL(url, filePath) {
+    request.get(url, filePath)
+      .on('error', function (err) {
+        console.log(err);
+        throw err;
+      })
+      .on('response', function (response) {
+        console.log((response.statusMessage), (response.headers['content-type']));
+      })
+      .pipe(fs.createWriteStream(filePath));
+  }
+
+//get the repo contributors api based on the name of the repo and the name of the owner of that repo
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        Authorization: "token " + secrets.GITHUB_TOKEN
+      },
+      json: true
+    };
+//download the image to the avatars folder using the users login for the name
+    request(options, function(err, res, body) {
+      cb(err, body);
+      body.forEach(function(item){
+        avatarUrl = item.avatar_url;
+        downloadImageByURL(avatarUrl, 'avatars/' + item.login + '.jpeg');
+      });
+    });
+  }
+//call the function
+  getRepoContributors(repoOwner, repoName, function() {});
+// if information missing tell the user to add the repo owner and name
 } else {
   console.log('Please define repo owner and name...')
 }
